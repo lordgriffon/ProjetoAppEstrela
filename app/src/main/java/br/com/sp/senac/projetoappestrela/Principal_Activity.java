94689f3074e3a31a1ebf0c7e4caef2f8fef5e07b
leonardo.prodrigue1 <leopr.95@hotmail.com>
2020-02-20 12:20:03 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/projetoappestrela/Principal_Activity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.projetoappestrela;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Principal_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal_layout);
+    }
+}
